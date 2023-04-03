2e0a2ed3812f93afa174604e65f2493ea04ffd36
=Minnas <=phannhattrieu01234@gmail.com>
2023-04-03 15:39:00 +0700

    add configuration file

---
+++ source/setting.py
@@ -0,0 +1,12 @@
+# game setting
+SCREEN_SIZE = (600, 800)
+TILESIZE = 128
+GAME_SIZE = 3
+FPS = 60
+TITLE = "Puzzle Searching Algorithm"
+
+# color
+BACKGROUND_COLOR = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+DARKGREY = (40, 40, 40)
+WHITE = (255, 255, 255)
